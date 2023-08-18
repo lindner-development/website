2f3b9d6fb4b3399b9e506de3c5bd7ed5285f3173
Nikola Hristov <nikola@nikolahristov.tech>
2023-08-18 16:55:45 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,9 @@ import solidJs from "@astrojs/solid-js";
 import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
 import compressor from "astro-compressor";
-import compress from "@otterlord/astro-compress";
-import critters from "@otterlord/astro-critters";
-//import rome from "@otterlord/astro-rome";
+import compress from "astro-compress";
+import critters from "astro-critters";
+import rome from "astro-rome";
 //import webmanifest from 'astro-webmanifest'; // TODO
 
 //import deno from '@astrojs/deno';
@@ -30,12 +30,12 @@ export default defineConfig({
     lastmod: new Date(),
     changefreq: 'monthly',
   }), critters(),
-  //rome(),
-  compress(), partytown({
+  rome(),
+  partytown({
     config: {
       forward: ["dataLayer.push"]
     }
-  }), compressor()],
+  }), compressor(), compress()],
   experimental: {
     //assets: true,
     //viewTransitions: true
